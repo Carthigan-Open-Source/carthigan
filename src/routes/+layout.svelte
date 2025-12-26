783efba73f10e2d3a5965ba3e28f8be28217974d
Eden Gilbert <edengilbertus@proton.me>
2025-12-26 11:28:38 +0300

    Add Carthigan favicon (charcoal C logo)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
 	import MobileMenu from '$lib/components/MobileMenu.svelte';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 <MobileMenu />
 {@render children()}
